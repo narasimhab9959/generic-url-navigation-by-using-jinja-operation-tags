de54fea1db49031cc5792802a984790453ff5eec
narasimhab9959 <narasimhab8555@gmail.com>
2023-06-01 18:18:48 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def ram(request):
+    return render(request,'ram.html')
+
+def sitha(request):
+    return render(request,'sitha.html')
